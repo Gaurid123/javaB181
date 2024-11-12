b6d9c7c2e2f5f1db4b241841e1aef1a2a9a57838
Dell <Dell@DESKTOP-UT87GUV>
2024-11-12 13:04:20 +0530

    by Piyush

--- DemoGittsproject/src/com/cjc/model/Githome.java
+++ DemoGittsproject/src/com/cjc/model/Githome.java
@@ -2,7 +2,12 @@ package com.cjc.model;
 
 public class Githome 
 {
+	public void m1()
+	{
+		System.out.println("This is m1 method");
+	}
 public static void main(String[] args) 
+
 {
 	System.out.println("This is git method");
 }
