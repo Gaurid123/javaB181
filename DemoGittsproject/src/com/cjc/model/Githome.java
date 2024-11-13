dc3d88c8c0db7963e211f1849c74ca22938aa872
Gaurid123 <gaurideshmukhofficial@gmail.com>
2024-11-13 14:25:37 +0530

    Update Githome.java

--- DemoGittsproject/src/com/cjc/model/Githome.java
+++ DemoGittsproject/src/com/cjc/model/Githome.java
@@ -10,6 +10,10 @@ public class Githome
 	{
 		System.out.println("This is m2 method");
 	}
+	public void m3()
+	{
+		System.out.println("This is m3 method");
+	}
 public static void main(String[] args) 
 
 {
